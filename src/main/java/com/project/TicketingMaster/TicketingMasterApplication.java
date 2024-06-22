4e56341097261c1528c81862caffe91d314b27ec
Vishnu Y S <ysv8152@gmail.com>
2024-06-22 12:20:04 +0530

    Initial Commit

---
+++ src/main/java/com/project/TicketingMaster/TicketingMasterApplication.java
@@ -0,0 +1,13 @@
+package com.project.TicketingMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingMasterApplication.class, args);
+	}
+
+}
